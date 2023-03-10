7f7e45d9548fa5d987b35f16d14f056d1349d7e6
Bharath2805 <115744883+Bharath2805@users.noreply.github.com>
2023-03-10 15:29:40 +0530

    Add files via upload
    simple java script weather app
    api from weatherapi.com

---
+++ weather app/script.js
@@ -0,0 +1,43 @@
+let data;
+
+
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temprature = document.getElementById("temprature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const getData = async (event) => {
+  event.preventDefault();
+  if (!inputBox.value) {
+    alert("Please Enter The City Name: ");
+    return;
+  }
+
+  //
+  const city = inputBox.value;
+
+  // Fetch Details
+
+  const fetchData = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=027702254b6340729cc90757231003&q=${city}`
+  );
+
+  const orgData = await fetchData.json();
+  data = orgData;
+  console.log(data);
+
+  // Displaying the data in HTML
+  countryName.innerHTML = data.location.country;
+  stateName.innerHTML = data.location.region;
+  cityName.innerHTML = data.location.name;
+  humidity.innerHTML = data.current.humidity;
+  windSpeed.innerHTML = data.current.wind_kph;
+  temprature.innerHTML = data.current.temp_c;
+  logoImage.src = data.current.condition.icon;
+  weatherStatus.innerHTML = data.current.condition.text;
+};
